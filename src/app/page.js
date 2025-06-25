a009cc99893027a53a157e6d945a06a124ee1a9d
siddhantgupta547 <sidgpt84@gmail.com>
2025-06-26 00:16:42 +0530

    Handled URL Pin modal

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
 import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 import styles from './page.module.css';
 import { fetchPinsData, handleDeleteAllClick } from '@/utils/apiCalls';
@@ -9,19 +10,6 @@ import FeedbackModal from '@/components/FeedbackModal';
 import Header from '@/components/Header';
 import LoadingOverlay from '@/components/LoadingOverlay';
 import MemoizedPin from '@/components/Pin';
-
-//Function to call fetch Pins API and then put array data into Map and then to pins state
-async function fetchAndSetPins(setterFn, loaderFn) {
-  loaderFn(true);
-  const data = await fetchPinsData();
-  const pins = data?.pins;
-  const pinsMap = new Map();
-  pins.forEach((pin) => {
-    pinsMap.set(pin?.id, pin);
-  });
-  setterFn(pinsMap);
-  loaderFn(false);
-}
 
 export default function Home() {
   //State Variables
@@ -32,11 +20,35 @@ export default function Home() {
 
   //router instance
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   //Effects
   useEffect(() => {
-    fetchAndSetPins(setPins, setFetchingPins);
+    fetchAndSetPins();
   }, []);
+
+  //Function to call fetch Pins API and then put array data into Map and then to pins state
+  //Checks for query params in URL
+  //if there is a pin ID then that specific modal is mounted
+  async function fetchAndSetPins() {
+    setFetchingPins(true);
+    const data = await fetchPinsData();
+    const pins = data?.pins;
+    const pinsMap = new Map();
+    pins.forEach((pin) => {
+      pinsMap.set(pin?.id, pin);
+    });
+    setPins(pinsMap);
+    setFetchingPins(false);
+    const pinIdFromUrl = searchParams.get('pinId');
+    if (pinIdFromUrl) {
+      const pinToOpen = pinsMap.get(Number(pinIdFromUrl));
+      if (pinToOpen) {
+        setModalPinData(pinToOpen);
+        setShowModal(true);
+      }
+    }
+  }
 
   // Handler to open modal for an existing pin
   const handlePinClick = useCallback(
